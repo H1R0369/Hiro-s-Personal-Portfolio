94e590a67ec8db1a715ccd11d31764a9a105227b
Hiro <sonamtopgey369@gmail.com>
2025-09-09 12:34:56 +0530

    Contain all gsap animations under the init function and add event listener to only play them after the website loads

--- Scripts/gsap.js
+++ Scripts/gsap.js
@@ -1,137 +1,142 @@
 // Logo Name Heading Animations
 
-gsap.set('.logo-name-heading', {
+function init() {
 
-    opacity: 0,
-    overflow: 'hidden'
+    gsap.set('.logo-name-heading', {
 
-})
+        opacity: 0,
+        overflow: 'hidden'
 
-gsap.fromTo('.main-container', {
+    })
 
-    autoAlpha: 0
+    gsap.fromTo('.main-container', {
 
-},
-{
+        autoAlpha: 0
 
-    autoAlpha: 1,
-    display: 'flex',
-    delay: 1,
-    duration: 2
+    },
+    {
 
-})
+        autoAlpha: 1,
+        display: 'flex',
+        delay: 1,
+        duration: 2
 
-gsap.to('.logo-name-heading', {
+    })
 
-    keyframes: [
+    gsap.to('.logo-name-heading', {
 
-        {
-            width: 300,
-            duration: 3,
-            ease: 'linear',
-            delay: 1.2,
-            startAt: {
-                width: 0,
-                opacity: 1
+        keyframes: [
+
+            {
+                width: 300,
+                duration: 3,
+                ease: 'linear',
+                delay: 1.2,
+                startAt: {
+                    width: 0,
+                    opacity: 1
+                }
+            },
+
+            {
+                textShadow: `
+                    0px 0px 5px var(--color-secondary),
+                    0px 0px 15px var(--color-secondary),
+                    0px 0px 20px var(--color-secondary),
+                    0px 0px 30px var(--color-secondary),
+                    0px 0px 40px var(--color-secondary),
+                    0px 0px 60px var(--color-secondary),
+                    0px 0px 98px var(--color-secondary)
+                `,
+                duration: 2,
             }
-        },
 
-        {
-            textShadow: `
-                0px 0px 5px var(--color-secondary),
-                0px 0px 15px var(--color-secondary),
-                0px 0px 20px var(--color-secondary),
-                0px 0px 30px var(--color-secondary),
-                0px 0px 40px var(--color-secondary),
-                0px 0px 60px var(--color-secondary),
-                0px 0px 98px var(--color-secondary)
-            `,
-            duration: 2,
-        }
+        ]
+    })
 
-    ]
-})
+    gsap.to('.logo-name-heading', {
 
-gsap.to('.logo-name-heading', {
+        overflow: 'visible',
+        delay: 2.5
 
-    overflow: 'visible',
-    delay: 2.5
+    })
 
-})
+    // Alias Heading Animations
 
-// Alias Heading Animations
+    const aliasEl = document.querySelector('.alias-heading');
+    const names = [
 
-const aliasEl = document.querySelector('.alias-heading');
-const names = [
+        'CODE WIZARD',
+        'FULLSTACK DEVELOPER',
+        'PYTHON ENTHUSIAST',
+        'GAME DEVELOPER',
+        'A SINGER',
+        'AN ARTIST'
 
-    'CODE WIZARD',
-    'FULLSTACK DEVELOPER',
-    'PYTHON ENTHUSIAST',
-    'GAME DEVELOPER',
-    'A SINGER',
-    'AN ARTIST'
+    ];
 
-];
+    const namesHTML = names.map(name => {
 
-const namesHTML = names.map(name => {
+        let nameHTML = '';
 
-    let nameHTML = '';
+        Array.from(name).forEach(letter => {
 
-    Array.from(name).forEach(letter => {
+            if (letter === ' ') {nameHTML += ' '}
+            else {nameHTML += `<span>${letter}</span>`};
 
-        if (letter === ' ') {nameHTML += ' '}
-        else {nameHTML += `<span>${letter}</span>`};
+        });
+
+        return nameHTML;
 
     });
 
-    return nameHTML;
+    let tl = gsap.timeline({defaults: {}, ease: 'linear', repeat: -1});
+    let idx = 0;
 
-});
+    setInterval(() => {
 
-let tl = gsap.timeline({defaults: {}, ease: 'linear', repeat: -1});
-let idx = 0;
+        aliasEl.innerHTML = namesHTML[idx]
+        idx++;
+        if (idx > names.length - 1) idx = 0;
 
-setInterval(() => {
+        gsap.from('.alias-heading span', {
 
-    aliasEl.innerHTML = namesHTML[idx]
-    idx++;
-    if (idx > names.length - 1) idx = 0;
+            visibility: 'hidden',
+            duration: 0.01,
+            stagger: 0.1,
+            ease: 'linear',
+            repeat: 1,
+            repeatDelay: 0.5,
+            yoyo: true
 
-    gsap.from('.alias-heading span', {
+        })
+        
+    }, 5000)
+        // .from(aliasEl, {
 
-        visibility: 'hidden',
-        duration: 0.01,
-        stagger: 0.1,
-        ease: 'linear',
-        repeat: 1,
-        repeatDelay: 0.5,
-        yoyo: true
+        //     innerHTML: namesHTML[1]
 
-    })
-    
-}, 5000)
-    // .from(aliasEl, {
+        // })
+        // .from(aliasEl, {
 
-    //     innerHTML: namesHTML[1]
+        //     innerHTML: namesHTML[2]
 
-    // })
-    // .from(aliasEl, {
+        // })
+        // .from(aliasEl, {
 
-    //     innerHTML: namesHTML[2]
+        //     innerHTML: namesHTML[3]
 
-    // })
-    // .from(aliasEl, {
+        // })
+        // .from(aliasEl, {
 
-    //     innerHTML: namesHTML[3]
+        //     innerHTML: namesHTML[4]
 
-    // })
-    // .from(aliasEl, {
+        // })
+        // .from(aliasEl, {
 
-    //     innerHTML: namesHTML[4]
+        //     innerHTML: namesHTML[5]
 
-    // })
-    // .from(aliasEl, {
+        // })
+}
 
-    //     innerHTML: namesHTML[5]
-
-    // })
+window.addEventListener('load', () => init());
